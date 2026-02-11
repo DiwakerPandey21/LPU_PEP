22a40e32a073719faa207f5f5272536420cba5bb
= <shivam.webcraft@gmail.com>
2026-02-11 12:17:01 +0530

    events

--- 5.react/3.forms/src/Products/Products.jsx
+++ 5.react/3.forms/src/Products/Products.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import './Products.css'
 
 const Products = (props) => {
+
+  // let price = props.price
+  const [price,setPrice] = useState(props.price);
+  
+  const priceHandler = () =>{
+    // console.log("I was clicked");
+    // price =100
+    // console.log(price)
+
+    setPrice(100);
+  }
+
   return (
     // figure tag display our content along with our image
-    <figure className='product'>
+    <figure className='product' onClick={priceHandler}>
         <img src={props.imgUrl} alt={props.name} />
         <h2>Name: {props.name}</h2>
-        <h3>Price: ${props.price}</h3>
+        <h3>Price: Rs {price}</h3>
     </figure>
   )
 }
