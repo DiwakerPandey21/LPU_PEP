98660a9feea0d7f65a8e431399db63fd971d77bd
DiwakerPandey21 <diwakerp050@gmail.com>
2026-02-12 11:16:38 +0530

    dp

---
+++ 5.react/my-react-app/src/Images1.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Images1() {
+  return (
+    <div>
+      <h1>Images in React</h1>
+      <img
+        src="https://cdn.pixabay.com/photo/2022/03/30/12/37/cars-7101111_1280.jpg"
+        alt="Lightning McQueen"
+        width="300"
+      />
+      <p>This is an example of an image in React.</p>
+    </div>
+  );
+}
+
+export default Images1;
